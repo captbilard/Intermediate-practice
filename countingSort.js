27721035b24b6ae59aa423eddd562e46f1690c25
bilard <bilardosinulu@gmail.com>
2023-01-19 16:44:18 -0700

    updated countingSort

--- countingSort.js
+++ countingSort.js
@@ -1,3 +1,5 @@
+//Another sorting method, the counting sort, does not require comparison. Instead, you create an integer array whose index range covers the entire range of values in your array to sort. Each time a value occurs in the original array, you increment the counter at that index. At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
+
 function countingSort(arr) {
     // Write your code here
     const freqArr = Array.from({length:arr.length}, (v,i) => i*0)
